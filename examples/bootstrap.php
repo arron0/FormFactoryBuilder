4b4d0a6c4d48c41423921b5cddbc6be7c2742ce3
Tomáš Lembacher <tomas.lembacher@seznam.cz>
2014-06-16 16:53:21 +0200

    There is a little pseudoexample.

---
+++ examples/bootstrap.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Load Nette
+ */
+
+/*
+$formBuilder = $container->getService('formBuilder.loader');
+$config = __DIR__ . '/example.neon';
+//$config = __DIR__ . '/register.neon';
+$formFactory = $formBuilder->load($config);
+$form = $formFactory->create(42, 24, 'some Parameter');
+
+echo '<html><body>';
+if($form->isSubmitted()) {
+	$form->validate();
+	dump($form->getValues());
+}
+echo((string)$form);
+echo '</body></html>';
+exit;
+*/
